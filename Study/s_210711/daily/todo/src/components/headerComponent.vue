dd5918eeb3d3baa1fb5c8e3a76bedfb9c9a55b94
DESKTOP-IO776H5\TJ <hyeona_c@naver.com>
2021-07-11 11:19:59 +0900

    rff\]

---
+++ Study/s_210711/daily/todo/src/components/headerComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="header">
+      <h1>HEADER</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+#header {width:100%; height: 100px; background: rgb(104, 105, 190); line-height: 100px; text-align: center;
+  h1{
+    font:20px;
+  }
+
+}
+
+</style>
